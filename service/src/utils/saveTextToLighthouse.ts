780a944351fe39a29420783c69edc02a8b029e4e
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2024-06-02 13:13:41 +0200

    Add lighthouse

---
+++ service/src/utils/saveTextToLighthouse.ts
@@ -0,0 +1,19 @@
+import lighthouse from '@lighthouse-web3/sdk'
+import { LighthouseRes } from '../data/lighthouse.data'
+import { getApiKey } from './getApiKey';
+
+/*
+// Sample response
+{
+  data: {
+    Name: 'shikamaru',
+    Hash: 'QmY77L7JzF8E7Rio4XboEpXL2kTZnW2oBFdzm6c53g5ay8',
+    Size: '91'
+  }
+}
+*/
+const saveTextToLighthouse = async(text:string):Promise<LighthouseRes> => {
+    const res = await getApiKey();
+    const response = await lighthouse.uploadText(text, res.data.apiKey)
+    return response.data as LighthouseRes
+}
